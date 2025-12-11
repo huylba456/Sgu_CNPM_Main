e1814f43807124a15fcee739899e1b2690658a03
chanvinh060104 <chibidethuong1@gmail.com>
2025-12-11 08:03:06 +0700

    Update

--- mobile/src/components/RevenueChart.js
+++ mobile/src/components/RevenueChart.js
@@ -112,7 +112,7 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 8,
     borderTopRightRadius: 8,
     minHeight: 8,
-    maxHeight: '100%'
+    maxHeight: '80%'
   },
   barLabel: {
     color: colors.textMuted,
